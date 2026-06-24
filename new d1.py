b189552d22a241327cd6f8be1fa691ee6b4e7355
Vishal Mishra <vishalmishra9207@gmail.com>
2026-06-24 11:52:36 +0530

    Add files via upload

---
+++ new d1.py
@@ -0,0 +1,37 @@
+# name = input('Eneter you name: ')
+# age = int(input('Enter age'))
+
+# print('Hello, My self ' ,name ,'and I am', age ,'years old')
+
+
+# a = int(input('Enter number: '))
+
+# b = int(input('Enter number: '))
+
+# s= a + b 
+
+# print('Sum of both number = ', s)
+
+
+# l = int(input('Enter length of rectangle: '))
+
+# b = int(input('Enter breadth of rectangle: '))
+
+# area = l * b 
+
+# print('Area of rectangle = ', area)
+
+
+# c = int(input('Enter temperature: '))
+
+# f =  (9/5) * c + 32
+
+# print(f)
+
+# n = int(input('Enter number: '))
+
+# square = n **2 
+# cube = n **3
+
+# print("square of number is = ", square)
+# print("cube of number is = ", cube)
