19916127d72e1c6a7f1b57f9c63f7403ec8bbac3
Vishal Mishra <vishalmishra9207@gmail.com>
2026-06-25 11:55:47 +0530

    Add files via upload

---
+++ new d2.py
@@ -0,0 +1,53 @@
+# n = int(input('Enter number: '))
+
+# if 0 == n %2:
+#     print('Even')
+# else:
+#     print('Odd')
+
+
+# n = int(input('Enter number:'))
+
+# if n > 0:
+#     print('possitive number')
+# elif n < 0:
+#     print('negetive number')
+# else:
+#     print("0")
+
+# age = int(input('Enter your age: '))
+
+# if age > 18 :
+#     print('Elgigble for voting')
+# else :
+#     print('Not elgigible')
+
+
+# a = int(input("Enter first number : "))
+
+# b = int(input("Enter second number: "))
+
+# if a > b :
+#     print('This number is bigger', a)
+# elif a == b:
+#     print('Both are equal')
+
+# else :
+#     print('This number is bigger', b )
+
+
+# a = int(input("Enter number: "))
+# b = int(input("Enter number: "))
+
+# op = input('Choose your operation(*,+,-,/):').strip()
+
+# if op == '*' :
+#     print(a*b)
+# elif op == '-':
+#     print(a-b)
+# elif op== '+':
+#     print(a+b)
+# elif op == '/':
+#     print(a % b)
+# else:
+#     print('Invalid operator')
