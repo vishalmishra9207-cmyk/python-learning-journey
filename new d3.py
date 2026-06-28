07959e4d5f6b551c865acaba3a92fba948721fbf
Vishal Mishra <vishalmishra9207@gmail.com>
2026-06-28 19:28:14 +0530

    Add files via upload

---
+++ new d3.py
@@ -0,0 +1,50 @@
+# a = int(input('Enter number: '))
+# b = int(input('Enter number: '))
+# c = int(input('Enter number: '))
+
+# if a > b and a > c:
+#     print('This number is bigger: ',a)
+# elif b > c :
+#     print('This number is bigger: ', b)
+# elif a == b and b == c :
+#     print('All number are equal')
+# else:
+#     print('This number is bigger: ', c)
+
+
+# n = int(input('Enter number: '))
+
+# if n % 5 == 0 and n % 11 == 0 :
+#     print('Number is divisble by 5 and 11')
+# else:
+#     # print('Number is not divisble by 5 and 11')
+
+# y = int(input('Enter year for leap year: '))
+
+# if y % 400 == 0:
+#     print('Leap year')
+# elif y % 100 == 0 :
+#     print('This is not a leap year')
+# elif y % 4 == 0 :
+#     print('Leap year')
+# else:
+#     print('Not a leap year')
+
+# marks = float(input("Enter number: "))
+
+# if marks >= 90 :
+#     print('Grade A')
+# elif marks >= 75 :
+#     print('Grade B')
+# elif marks >= 50 :
+#     print('Grade C')
+# else :
+#     print('Fail')
+
+# user_name = input('Enter name: ')
+# password = int(input('Enter password: '))
+
+# if user_name == 'admin' and password == 1234:
+#     print('Login succesful')
+# else:
+#     print('Invalid user_name or password')
