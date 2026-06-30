9e6c8ebef8c9ee1174c144b21e05d8aa0d337bf2
Vishal Mishra <vishalmishra9207@gmail.com>
2026-06-30 22:01:15 +0530

    Add files via upload

---
+++ new d5.py
@@ -0,0 +1,20 @@
+# # for i in range(1,5):
+# #     for j in range(1,5):
+# #         print('*', end=" ")
+# #     print()
+
+# # for i in range(5,0,-1):
+# #     for j in range(i):
+# #         print('*', end=" ")
+# #     print()
+
+
+# # for i in range(1,11):
+# #     if i == 5:
+# #         continue
+# #     print(i)
+
+# for i in range(1,11):
+#     if i == 7:
+#         break
+#     print(i)
