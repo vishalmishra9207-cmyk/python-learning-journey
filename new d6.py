c4292298637df342f5585e4b142c229ef7adada5
Vishal Mishra <vishalmishra9207@gmail.com>
2026-07-01 22:00:22 +0530

    Add files via upload

---
+++ new d6.py
@@ -0,0 +1,68 @@
+# n = int(input('Enter number: '))
+
+# cd = 0
+
+# while (n > 0):
+#     r = n % 10
+#     n = n // 10
+#     cd += 1 
+
+# print('Total digits: ', cd)
+
+
+# n = int(input('Enter number: '))
+
+# sum  = 0
+
+# while (n > 0):
+#     sum = sum+ n % 10
+#     n = n // 10
+
+# print('Total digits: ', sum)
+
+
+# n = int(input('Enter number: '))
+
+# rev  = 0
+
+# while (n > 0):
+#     r =  n % 10
+#     rev = rev * 10 + r
+#     n = n // 10
+
+# print('Reverse: ', rev)
+
+# n = int(input('Enter number: '))
+
+# rev  = 0
+
+# temp = n 
+
+# while (temp > 0):
+#     r =  temp % 10
+#     rev = rev * 10 + r
+#     temp = temp // 10
+
+# print("Reverse : ", rev)  
+
+
+# if n == rev :
+#     print("Palindrome")
+# else:
+#     print('Not Palindrome')
+
+# n = int(input("Enter number : "))
+
+# o = n 
+
+# sum = 0 
+
+# while n > 0 :
+#     digit = n % 10 
+#     sum += digit ** 3
+#     n //= 10 
+
+# if sum == o :
+#     print('Armstrong')
+# else:
+#     print("Not Armstrong")
