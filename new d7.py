b2e5424738876c8d25272cf466a041c01ccb78d0
Vishal Mishra <vishalmishra9207@gmail.com>
2026-07-02 21:44:01 +0530

    Add files via upload

---
+++ new d7.py
@@ -0,0 +1,82 @@
+# # # # name = input('Enter your name : ')
+
+# # # # print(name[0])
+
+# # # # print(name[-1])
+
+# # # # print(name [0:3])
+
+# # # # print(len(name))
+
+# # # # print(name.upper())
+
+
+# # # # name = input('Enter name : ')
+
+# # # # print(len(name))
+
+# # # # print(name[-1:-7:-1])
+
+# # # word = input("Enter you word: ")
+
+# # # copy_word1  = list(word)
+
+# # # copy_word1.reverse()
+
+# # # if list(word) == copy_word1:
+# # #     print('Palindrome')
+# # # else:
+# # #     print('Not a palindrome')
+
+
+# # # word = input("Enter your word: ")
+
+# # # copy_word1 = list(word)        # string ko list of chars banao
+# # # copy_word1.reverse()           # reverse karo
+
+# # # if list(word) == copy_word1:
+# # #     print("Palindrome")
+# # # else:
+# # #     print("Not a palindrome")
+
+
+# # # name = input("Enter name: ")
+
+# # # vowels = "aeiouAEIOU"
+# # # count = 0
+
+# # # for char in name:
+# # #     if char in vowels:
+# # #         count += 1
+
+# # # if count > 0:
+# # #     print("Number of vowels:", count)
+# # # else:
+# # #     print("No vowels in the given name")
+
+
+# # name = input("Enter name: ")
+
+# # vowels = "aeiouAEIOU"
+# # count = 0
+
+# # for ch in name:
+# #     if ch in vowels:
+# #         count += 1
+
+# # print("Number of vowels:", count)
+
+# name = input("Enter name: ")
+
+# vowels = "aeiouAEIOU"
+# count = 0
+
+# for char in name:
+
+#     if char.isalpha() and char not in vowels:
+#         count += 1
+
+# print("Consonants =", count)
+
+
+ 
