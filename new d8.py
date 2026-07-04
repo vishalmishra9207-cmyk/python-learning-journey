c992c7b035f12a12420bd4a6a2ff0c2acda3c588
Vishal Mishra <vishalmishra9207@gmail.com>
2026-07-04 22:45:59 +0530

    Add files via upload

---
+++ new d8.py
@@ -0,0 +1,84 @@
+# # # list = [10,20,30,40]
+
+# # # print(list[0], list[len(list)-1])
+
+# # # num = [10,20,30]
+
+# # # num.append(40)
+
+# # # print(num)
+
+
+# # # number = [10,20,30,40]
+
+# # # number.remove(20)
+
+# # # print(number)
+
+# # # n= [10,20,30,40]
+
+# # # n.pop()
+
+# # # print(n)
+
+
+# # # s = [5,10,15,20]
+
+# # # print(sum(s))
+# # # print(max(s))
+# # # print(min(s))
+
+# # # nums = [10,20,30,40,50]
+
+# # # sum = 0 
+
+# # # for items in nums:
+# # #     sum += items
+
+# # # print('Total sum : ', sum)
+
+# # # nums = [10, 45, 22, 99, 15]
+
+# # # l = 0 
+
+
+# # # biggest_num = nums[0]
+
+
+# # # for item in nums:
+# # #     l += 1 
+# # #     if item > biggest_num:
+# # #      biggest_num = item
+
+# # # print('Biggest number = ', biggest_num)4
+
+# # # nums = [10, 45, 22, 99, 15]
+
+# # # smallest_num = nums[0]
+
+# # # for item in nums:
+     
+# # #     if item < smallest_num:
+# # #      smallest_num_num = item
+
+# # # print('Smallest number = ', smallest_num)
+
+# # nums = [10, 15, 20, 33, 40, 51]
+
+# # even_nums = []
+
+# # for item in nums:
+# #     if item % 2 == 0:
+# #         even_nums.append(item)
+
+# # print(even_nums)
+
+# nums = [10, 15, 20, 33, 40, 51]
+
+# odd_nums = []
+
+# for item in nums:
+#     if item % 2 != 0:
+#         odd_nums.append(item)
+
+# print(odd_nums)
