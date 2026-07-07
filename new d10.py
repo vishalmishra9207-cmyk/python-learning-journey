d7978a2b5113f1cfb28d18ceaa51d417f74deca7
Vishal Mishra <vishalmishra9207@gmail.com>
2026-07-07 22:47:11 +0530

    Add files via upload

---
+++ new d10.py
@@ -0,0 +1,61 @@
+# # # def table(n):
+# # #     for i in range(1,11):
+# # #         print(n*i)
+
+# # # table(3)
+
+# # # def calc_sum(s):
+# # #     total= 0
+# # #     for i in range(1,s+1):
+# # #         total += i
+# # #     print(total)
+    
+# # # calc_sum (6)
+
+
+# # # nums = [10, 15, 20, 33, 40]
+
+# # # def even_num(nums):
+# # #     count = 0 
+# # #     for i in (nums):
+# # #         if i % 2 == 0:
+# # #             count += 1 
+# # #     print("Total Even Count:", count)
+
+# # # even_num(nums)
+
+
+# # nums = [10, 45, 22, 99, 15]
+
+# # def largest_num (nums):
+# #     largest = nums[0]
+# #     for items in nums :
+
+# #         if items > largest:
+# #             largest = items 
+# #     return largest 
+
+# # print(largest_num(nums))
+
+# # def prime_number (n):
+# #     if n > 1:
+# #        for i in range (2,n):
+# #            if n % i == 0 :
+# #                return False
+# #        return True
+# #     else:
+# #         return True
+
+# # prime_number(16)
+
+# def prime_number(n):
+#     if n > 1:
+#         for i in range(2, n):
+#             if n % i == 0:
+#                 return "Prime number nhi hai "
+#         return "Prime number hai "
+#     else:
+#         return "Prime number nhi hai"
+
+# print(prime_number(99)) 
+
