c5af3bf24e4c1b2f1cb25383b086d884188757c6
Vishal Mishra <vishalmishra9207@gmail.com>
2026-07-09 12:22:03 +0530

    Add files via upload

---
+++ new d9.py
@@ -0,0 +1,41 @@
+# # name = input("Enter you name:")
+
+# # def greet (name):
+# #     print("hello ",name)
+
+# # greet("Rahul")
+
+
+
+# def square(n):
+#    return n**2
+# result = square(12658)
+
+# print(result)
+
+# def is_even(n):
+#     return n % 2 == 0
+# n = int(input("Enter you number : "))
+
+# result = is_even(n)
+
+# if result :
+#     print("Even")
+# else:
+#     print("Odd")
+
+# def is_even(n):
+#  if n % 2 == 0:
+#     print("Even")
+#  else:
+#     print("Odd")  kachra code 
+
+def largest (a,b):
+    if a > b:
+     return a
+    elif a == b :
+      return "both number are equal"
+    else:
+     return b
+    
+print(largest(20,20))
