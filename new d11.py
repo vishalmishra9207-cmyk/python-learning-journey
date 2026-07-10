5d2405dacc62f206bd4c1e933dc1ca2cb3592284
Vishal Mishra <vishalmishra9207@gmail.com>
2026-07-10 23:18:08 +0530

    Add files via upload

---
+++ new d11.py
@@ -0,0 +1,63 @@
+# nums = [10, 15, 20, 33, 40, 51]
+
+# def count_odds(nums):
+#     count = 0 
+
+#     for i in (nums):
+#         if i % 2 != 0 :
+#             count += 1 
+#     return count
+        
+# print(count_odds(nums))
+
+
+# def count_vowels(word):
+#     vowels = "aeiouAEIOU"
+#     consonant_count = 0
+#     for char in word:
+#         if char.isalpha() and char  in vowels:   # vowel ke alawa
+#          consonant_count += 1
+
+# print("Vowels:", count_vowels)
+
+# def count_vowles(word):
+#    vowles = "aeiouAEIOU"
+#    count = 0
+#    for char in word:
+#       if char.isalpha() and char in vowles:
+#          count +=1
+#    return count
+
+# name = input("Enter you name : ")
+# print("Vowles count:",count_vowles(name))
+
+
+# nums = [45, 12, 78, 3, 99]
+
+# def smallest_num(nums):
+#     smallest = nums[0]
+#     for items in nums:
+#         if items < smallest:
+#          smallest = items
+#     return smallest
+
+# print(smallest_num(nums))
+
+# # prime_number(16)
+
+# def prime_number(n):
+#     if n > 1:
+#         for i in range(2, n):
+#             if n % i == 0:
+#                 return "Prime number nhi hai "
+#         return "Prime number hai "
+#     else:
+#         return "Prime number nhi hai"
+
+# print(prime_number(99)) 
+
+
+# def  reverse_string(word):
+#     return "".join(reversed(word))
+
+# print(reverse_string("Python"))
