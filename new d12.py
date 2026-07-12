c9470a541e5122aa49344e5e6960300212e4025a
Vishal Mishra <vishalmishra9207@gmail.com>
2026-07-12 22:42:56 +0530

    Add files via upload

---
+++ new d12.py
@@ -0,0 +1,66 @@
+# nums = [10, 75, 32, 90, 45, 60]
+
+
+# def count_greater_than_50(nums):
+#     count_greater_50 = 0
+#     for items in nums:
+#         if items > 50 :
+#             count_greater_50 += 1
+#     return count_greater_50
+
+# print(count_greater_than_50(nums))
+
+
+# nums = [-5, 10, -2, 8, 0, 15]
+
+# def positive_numbers(nums):
+#     poss_num = []
+#     for items in nums:
+#         if items > 0 :
+#             poss_num.append(items)
+#     return poss_num
+
+# result = positive_numbers(nums)
+
+# print("Possitive nnumbers : ", result)
+# print("Toatal hai : ", len(result))
+
+
+# word = "banana"
+ 
+# def word_count():
+#     max_count = 0   
+#     for item in word:
+#      freq = word.count(item)
+
+#     if freq > max_count:
+#        freq = word
+    
+# print(word_count())
+
+    
+# nums = [10, 20, 30, 40]
+
+# def avg_nums(nums):
+#     return sum(nums)/ len(nums)
+
+# print(avg_nums(nums))
+
+# def fibonachi(n):
+#     fib = []
+#     a,b = 0, 1 
+#     for i in  range (n):
+#         fib.append(a)
+#     a , b = b , a + b 
+
+# fibonachi(7)
+
+# def fibonachi(n):
+#     fib = []
+#     a,b = 0, 1 
+#     for i in range (n):
+#         fib.append(a)
+#         a,b = b, a + b 
+#     return fib 
+
+# print(fibonachi(7))
