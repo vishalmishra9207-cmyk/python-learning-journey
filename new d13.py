25cc4030362796cc8b3b7cb1855eb6ce4283083a
Vishal Mishra <vishalmishra9207@gmail.com>
2026-07-15 23:22:45 +0530

    Add files via upload

---
+++ new d13.py
@@ -0,0 +1,30 @@
+# student = {
+#     "name": "Vishal",
+#     "age": 24,
+#     "city": "Delhi"
+# }
+
+# # print(student["name"])
+
+# student.update({"course": "Python"})
+
+# # student["age"] = 25
+
+# # print(student.keys())
+
+# # print(student.values())
+
+# print(len(student))
+
+
+# marks = {
+#     "Math": 85,
+#     "Physics": 90,
+#     "Chemistry": 78
+# }
+
+
+# subject  = max(marks,key=marks.get)
+# max_marks = marks[subject]
+
+# print(subject,max_marks)
