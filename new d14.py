15e9f354429566d6d70f8759ac38829d56ca82ed
Vishal Mishra <vishalmishra9207@gmail.com>
2026-07-23 21:19:11 +0530

    Add files via upload

---
+++ new d14.py
@@ -0,0 +1,44 @@
+# student = {
+#     "name": "Vishal",
+#     "age": 25,
+#     "city": "Delhi"
+# }
+
+# # q1
+# if 'age' in student :
+#     print("Hai BC")
+# else :
+#     print("Nhi hai BC ")
+
+# # q2
+# for key, value in student.items():
+#     print(key, value)
+
+# marks = {
+#     "Math": 85,
+#     "Physics": 90,
+#     "Chemistry": 78,
+#     "English": 88
+# }
+
+
+# #q4
+# avg_marks  = sum(marks.values())/len(marks)
+
+# print(avg_marks)
+
+# # q5
+# print(len(marks))
+
+# print(max(marks.values()))
+
+# heighest_subject = None
+# heighest_marks = 0 
+
+# for subject, marks in marks.items():
+#     if heighest_subject is None or marks > heighest_marks:
+#         heighest_subject = subject
+#         heighest_marks = marks
+
+# print(heighest_subject,heighest_marks)
+
